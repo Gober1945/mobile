93b08e64d87531913d9fc31a359f78aa78b8eade
unknown <afriarevfer29@gmail.com>
2025-08-26 11:24:45 +0700

    Update riwayat.dart dan transfer.dart

--- lib/riwayat.dart
+++ lib/riwayat.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/transfer.dart';
-
 
 class RiwayatPage extends StatelessWidget {
   const RiwayatPage({super.key});
--- lib/transfer.dart
+++ lib/transfer.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'riwayat.dart';
 
 void main() {
   runApp(const MyApp());
