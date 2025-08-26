10b2eedf5cc7ab796fdc2f8b7da52f10be4f23da
unknown <afriarevfer29@gmail.com>
2025-08-26 11:50:17 +0700

    Update kode Flutter

--- lib/transfer.dart
+++ lib/transfer.dart
@@ -25,17 +25,17 @@ class TransferPage extends StatelessWidget {
             Container(
               padding: const EdgeInsets.all(16),
               color: Colors.grey[200],
-              child: Row(
+              child: const Row(
                 children: [
-                  const CircleAvatar(
+                  CircleAvatar(
                     radius: 24,
                     backgroundColor: Colors.grey,
                     child: Icon(Icons.person, color: Colors.white),
                   ),
-                  const SizedBox(width: 12),
+                  SizedBox(width: 12),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
+                    children: [
                       Text(
                         'Ahmad fauzan Hafiz Zaulloh',
                         style: TextStyle(fontWeight: FontWeight.bold),
